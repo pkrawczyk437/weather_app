eeaa8c031d3006566b2fb5c2a1b847bd2874a8f4
pkrawczyk437 <47972435+pkrawczyk437@users.noreply.github.com>
2020-04-14 10:44:29 +0200

    Add https for API URL

--- src/client/getWeather.js
+++ src/client/getWeather.js
@@ -1,5 +1,5 @@
 
 const apiKey = "7e5885c5cb53a7df21d2de845f70342e";
-const apiURL = `http://api.openweathermap.org/data/2.5/weather?q=`;
+const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=`;
 
 export const getWeather = (city, countryCode) => fetch(`${apiURL}${city},${countryCode}&units=metric&appid=${apiKey}`).then(res => res.json())
